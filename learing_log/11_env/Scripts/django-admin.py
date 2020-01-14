ac8238587ea5ac7b43a4863e4a1a82926fbc0879
eddie.wang@1mabc.com <eddie.wang@1mabc.com>
2020-01-14 15:19:46 +0800

    [ADD]Django入门-学习笔记项目

---
+++ learing_log/11_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\gitpython\python_study\learing_log\11_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ learing_log/learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""定义learning_logs的URL模式"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # 主页
+    url(r'^$', views.index, name='index'),
+
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # 显示特定主题的详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+]
